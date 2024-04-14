0d03c517c95159a5f7eeb510238452c8e98c76c7
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2024-04-14 15:59:04 +0530

    Functions

--- JS LEVEL 2/script.js
+++ JS LEVEL 2/script.js
@@ -3,6 +3,8 @@
 /* ab = 21;
 console.log(ab); */
 
+/*
+
 let hasDriverLicense = false;
 const passTest = true;
 
@@ -10,3 +12,69 @@ if (passTest) hasDriverLicense = true;
 if (hasDriverLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
+// const private = 534;
+// const if = 23;
+*/
+
+function logger() {
+  console.log("My name is bhavya");
+}
+
+// Calling
+// Running
+// invoking function    DOUBT
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+
+// function declaration
+const age1 = calcAge1(1991);
+
+function calcAge1(birthYear) {
+  //   const age = 2037 - birthYear;
+  //   return age;
+  return 2037 - birthYear;
+}
+
+console.log(age1);
+// const age1 = calcAge1(1991);
+
+// function expression
+// const age2 = calcAge2(1991);
+
+const calcAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+console.log(calcAge2);
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(2001, "Bhavya"));
+console.log(yearsUntilRetirement(1999, "Sarah"));
